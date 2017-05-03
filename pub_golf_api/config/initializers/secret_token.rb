24714a9ecbe6738bdee7819900c53302d776fe38
Kaitlyn Mohr <kpmohr@gmail.com>
2017-05-03 14:03:48 -0400

    ugh

---
+++ pub_golf_api/config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+PubGolfApi::Application.config.secret_token = '0822078ec38830acac77c8ac15757fe68403bd25999e9601f26eaeae33f3469e370fc6e6f133c28b6ff86defbbf465226852caea3eed5effafadae5acd3c9112'
